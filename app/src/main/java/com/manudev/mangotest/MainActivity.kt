e926c7c3b10c146661b5a7db24a77c94fbce1f2c
Manu Gutiérrez <manuel.gutierrez@abancagen.com>
2024-04-30 10:36:28 +0200

    [CORE] Add compose implementation in presentation gradle.

--- app/src/main/java/com/manudev/mangotest/MainActivity.kt
+++ app/src/main/java/com/manudev/mangotest/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.manudev.mangotest.ui.theme.MangoTestTheme
+import com.manudev.presentation.screens.theme.MangoTestTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
