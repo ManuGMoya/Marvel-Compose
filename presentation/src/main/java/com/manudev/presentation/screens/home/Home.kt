7914e8df0082f3f719be4d30ba2d11a87d5b1f16
Manu Gutiérrez <manuel.gutierrez@abancagen.com>
2024-05-06 12:55:48 +0200

    [REFACTOR] Refactor Home, extract values to constants.

--- presentation/src/main/java/com/manudev/presentation/screens/home/Home.kt
+++ presentation/src/main/java/com/manudev/presentation/screens/home/Home.kt
@@ -1,11 +1,10 @@
 package com.manudev.presentation.screens.home
 
-import android.annotation.SuppressLint
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,8 +14,6 @@ import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
@@ -40,98 +37,40 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.manudev.domain.model.CharacterDomain
 import com.manudev.presentation.R
 import com.manudev.presentation.screens.Screen
+import com.manudev.presentation.theme.CharacterImageSize
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+private const val SEARCH_DELAY = 400L
+private const val INITIAL_PAGE = 0
+private const val PAGE_SIZE = 20
+
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel(),
     onItemClick: (Int) -> Unit
 ) {
     val listState = rememberLazyListState()
-    var isSearchMode by remember { mutableStateOf(false) }
-
-    var searchText by remember { mutableStateOf("") }
-    val searchJob = rememberCoroutineScope()
 
     Scaffold(
         topBar = {
-            Crossfade(
-                targetState = isSearchMode,
-                label = "",
-                animationSpec = tween(1000)
-            ) { searchMode ->
-                TopAppBar(
-                    title = {
-                        if (searchMode) {
-                            TextField(
-                                value = searchText,
-                                onValueChange = { newValue ->
-                                    searchText = newValue
-                                    searchJob.launch {
-                                        delay(400)
-                                        if (newValue == searchText) {
-                                            if (newValue.isEmpty()) {
-                                                viewModel.getCharacters(0, 20, true)
-                                            } else {
-                                                viewModel.getCharacterByName(0, 20, newValue)
-                                            }
-                                        }
-                                    }
-                                },
-                                label = { Text(stringResource(R.string.Search)) },
-                                singleLine = true,
-                                keyboardActions = KeyboardActions(onDone = {
-                                    searchJob.launch {
-                                        viewModel.getCharacterByName(0, 20, searchText)
-                                    }
-                                }),
-                                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .background(Color.Transparent)
-                            )
-                        } else {
-                            Text(text = stringResource(R.string.AppName))
-                        }
-                    },
-                    actions = {
-                        IconButton(onClick = {
-                            isSearchMode = !isSearchMode
-                        }) {
-                            if (searchMode) {
-                                Icon(
-                                    imageVector = Icons.Default.Close,
-                                    contentDescription = stringResource(R.string.Close)
-                                )
-                            } else {
-                                Icon(
-                                    imageVector = Icons.Default.Search,
-                                    contentDescription = stringResource(R.string.Search)
-                                )
-                            }
-                        }
-                    }
-                )
-            }
+            HomeTopBar(
+                viewModel
+            )
         },
-        content = {
+        content = { paddingValues ->
             HomeContent(
                 listState,
                 viewModel.state,
+                paddingValues,
                 onItemClick = onItemClick,
                 onRefreshList = viewModel::getCharacters
             )
@@ -139,7 +78,78 @@ fun HomeScreen(
     )
 
     LaunchedEffect(Unit) {
-        viewModel.getCharacters(0, 20, true)
+        viewModel.getCharacters(INITIAL_PAGE, PAGE_SIZE, true)
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun HomeTopBar(
+    viewModel: HomeViewModel
+) {
+    var isSearchMode by remember { mutableStateOf(false) }
+    var searchText by remember { mutableStateOf("") }
+    val searchJob = rememberCoroutineScope()
+
+
+    Crossfade(
+        targetState = isSearchMode,
+        label = "",
+        animationSpec = tween(1000)
+    ) { searchMode ->
+        TopAppBar(
+            title = {
+                if (searchMode) {
+                    TextField(
+                        value = searchText,
+                        onValueChange = { newValue ->
+                            searchText = newValue
+                            searchJob.launch {
+                                delay(SEARCH_DELAY)
+                                if (newValue == searchText) {
+                                    if (newValue.isEmpty()) {
+                                        viewModel.getCharacters(
+                                            INITIAL_PAGE,
+                                            PAGE_SIZE,
+                                            true
+                                        )
+                                    } else {
+                                        viewModel.getCharacterByName(
+                                            INITIAL_PAGE,
+                                            PAGE_SIZE,
+                                            newValue
+                                        )
+                                    }
+                                }
+                            }
+                        },
+                        label = { Text(stringResource(R.string.Search)) },
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    )
+                } else {
+                    Text(text = stringResource(R.string.AppName))
+                }
+            },
+            actions = {
+                IconButton(onClick = {
+                    isSearchMode = !isSearchMode
+                }) {
+                    if (searchMode) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = stringResource(R.string.Close)
+                        )
+                    } else {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = stringResource(R.string.Search)
+                        )
+                    }
+                }
+            }
+        )
     }
 }
 
@@ -147,6 +157,7 @@ fun HomeScreen(
 fun HomeContent(
     listState: LazyListState,
     state: HomeViewModel.UiState,
+    paddingValues: PaddingValues,
     onItemClick: (Int) -> Unit,
     onRefreshList: (Int, Int) -> Unit
 ) {
@@ -167,16 +178,16 @@ fun HomeContent(
             else -> {
                 LazyColumn(
                     state = listState,
-                    modifier = Modifier.padding(top = 56.dp)
+                    modifier = Modifier.padding(paddingValues)
                 ) {
                     itemsIndexed(state.characters) { index, character ->
-                        if (index > 0) Divider()
+                        if (index > INITIAL_PAGE) Divider()
                         CharacterItem(
                             character = character,
                             onItemClick = { onItemClick(character.id) }
                         )
                         if (index == state.characters.size - 1) {
-                            onRefreshList(index + 1, 20)
+                            onRefreshList(index + 1, PAGE_SIZE)
                             LaunchedEffect(Unit) {
                                 listState.scrollToItem(index)
                             }
@@ -203,13 +214,13 @@ fun CharacterItem(
                     .build(),
                 contentDescription = character.name,
                 modifier = Modifier
-                    .size(50.dp)
+                    .size(CharacterImageSize)
                     .clip(CircleShape),
             )
         },
         supportingContent = {
             Text(
-                text = "${stringResource(R.string.Search)} ${character.numberOfComics}"
+                text = "${stringResource(R.string.NumberOfComics)} ${character.numberOfComics}"
             )
         },
         modifier = Modifier
---
+++ presentation/src/main/java/com/manudev/presentation/theme/Dimens.kt
@@ -0,0 +1,5 @@
+package com.manudev.presentation.theme
+
+import androidx.compose.ui.unit.dp
+
+val CharacterImageSize = 50.dp
