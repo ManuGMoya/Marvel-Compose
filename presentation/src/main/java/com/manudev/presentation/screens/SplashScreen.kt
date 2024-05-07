f054151268b8db64a70f244c6d363054c8c77f50
Manu Gutiérrez <manuel.gutierrez@abancagen.com>
2024-05-07 09:30:04 +0200

    [FEATURE] Add Animation to Splash.

--- presentation/src/main/java/com/manudev/presentation/screens/SplashScreen.kt
+++ presentation/src/main/java/com/manudev/presentation/screens/SplashScreen.kt
@@ -1,26 +1,36 @@
 package com.manudev.presentation.screens
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import com.manudev.presentation.R
 import kotlinx.coroutines.delay
 
-private const val DELAY_TIME = 2000L
+private const val ANIMATION_DELAY = 500L
+private const val ROTATION_DURATION = 2000
+private const val NAVIGATION_DELAY = 3000L
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
     Splash()
     LaunchedEffect(true) {
-        delay(DELAY_TIME)
+        delay(NAVIGATION_DELAY)
         navController.popBackStack()
         navController.navigate(Screen.Home.route)
     }
@@ -28,16 +38,29 @@ fun SplashScreen(navController: NavHostController) {
 
 @Composable
 fun Splash() {
+    var isAnimating by remember { mutableStateOf(true) }
+
+    val rotationAngle by animateFloatAsState(
+        targetValue = if (isAnimating) 360f else 0f,
+        animationSpec = tween(durationMillis = ROTATION_DURATION, easing = LinearEasing), label = ""
+    )
+
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .rotate(rotationAngle),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
-
     ) {
         AsyncImage(
             model = R.drawable.logo,
             contentDescription = stringResource(R.string.logo)
         )
         Text(text = stringResource(R.string.welcome_to_marvel_world))
+    }
+
+    LaunchedEffect(key1 = true) {
+        delay(ANIMATION_DELAY)
+        isAnimating = false
     }
 }
